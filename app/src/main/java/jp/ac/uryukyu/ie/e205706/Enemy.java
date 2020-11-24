7853a7ca9e04e780d5f7f0cce302659d83c672f6
MUTO KAKU <e205706@ie.u-ryukyu.ac.jp>
2020-11-25 03:57:33 +0900

    4th commit

--- app/src/main/java/jp/ac/uryukyu/ie/e205706/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205706/Enemy.java
@@ -9,10 +9,10 @@ package jp.ac.uryukyu.ie.e205706;
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
-    public String name;
-    public int hitPoint;
-    public int attack;
-    public boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -36,7 +36,7 @@ public class Enemy {
     public void attack(Hero hero){
         if (this.dead != true){
             int damage = (int)(Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.name, damage);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
             hero.wounded(damage);
         }
     }
@@ -52,6 +52,38 @@ public class Enemy {
             dead = true;
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String _name){
+        this.name = _name;
+    }
+
+    public int getHitPoint(){
+        return this.hitPoint;
+    }
+
+    public void setHitPoint(int _hitPoint){
+        this.hitPoint = _hitPoint;
+    }
+
+    public int getAttack(){
+        return this.attack;
+    }
+
+    public void setAttack(int _attack){
+        this.attack = _attack;
+    }
+
+    public boolean getDead(){
+        return this.dead;
+    }
+
+    public void setDead(boolean _dead){
+        this.dead = _dead;
     }
 
 }
